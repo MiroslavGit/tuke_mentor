9da734a7c90f18926a59871f1758e0f9ae068a2c
MiroslavGit <miroslav2022@gmail.com>
2024-01-30 21:18:20 +0100

    added back global.css to make tailwind works

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
+    <header className="fixed top-0 left-0 w-full bg-blue-500 text-white shadow-md z-10">
+      <nav className="container mx-auto">
+        <ul className="flex p-4">
+          <li className="mr-6">
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className="mr-6">
             <Link href="/bc">Bc.</Link>
           </li>
           <li>
@@ -17,5 +17,5 @@ export default function Header() {
         </ul>
       </nav>
     </header>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <Layout>
       <Header />
-      <h1>Welcome to My Website!</h1>
-      <p>This website is designed to help future students in the field of Informatics.</p>
+      <section className="container mx-auto mt-16">
+        <h1 className="text-3xl font-bold">Welcome to My Website!</h1>
+        <p>This website is designed to help future students in the field of Informatics.</p>
+      </section>
     </Layout>
   );
 }
